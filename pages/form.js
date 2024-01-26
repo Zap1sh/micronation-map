17d082fe81e8ccea2f222533f4eab0a2209ef3f4
Zap1sh <157256959+Zap1sh@users.noreply.github.com>
2024-01-25 18:45:39 -0800

    Update form.js

--- pages/form.js
+++ pages/form.js
@@ -16,7 +16,7 @@ export default function Home() {
           Please Fill out the Form Below to add your Micronation.
         </p>
 
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeInarN5XBS8BRQBl7RGMVI6hQu76tyIkNRRw6Za56jYVDvwA/viewform?embedded=true" width="640" height="840" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeInarN5XBS8BRQBl7RGMVI6hQu76tyIkNRRw6Za56jYVDvwA/viewform?embedded=true" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </main>
 
     </div>
